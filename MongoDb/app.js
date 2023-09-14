b5156b736599e3fd0f128564e35cb05020705cb9
AyaMouawi <ayamouawi8@gmail.com>
2023-09-14 14:42:02 +0300

    MongoDb Challenge 3 Done

--- MongoDb/app.js
+++ MongoDb/app.js
@@ -16,3 +16,27 @@ app.get("/api", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ayamouawi8:VFlVq62QfvkZEJ5s@cluster0.bzyvclr.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
